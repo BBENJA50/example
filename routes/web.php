880d9b94a47c9110b171bbf5e83f6e529ff8f943
bbenja50 <migom@hotmail.be>
2024-10-27 17:54:00 +0100

    added 30dtll 07 jobs

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,39 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+
+
+$jobs  = [
+    [
+        'id' => 1,
+        'title' => 'Web Developer',
+        'salary' => '$50,000',
+    ],
+    [
+        'id' => 2,
+        'title' => 'Software Developer',
+        'salary' => '$60,000',
+    ],
+    [
+        'id' => 3,
+        'title' => 'Software Engineer',
+        'salary' => '$80,000',
+    ],
+];
 
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/about', function () {
-    return view('about');
+Route::get('/jobs', function () use ($jobs) {
+    return view('jobs', ['jobs' => $jobs,]);
+});
+
+Route::get('/jobs/{id}', function ($id) use ($jobs) {
+    $job = Arr::first($jobs, fn($job) => $job['id'] = $id);
+
+    return view('job', ['job' => $job]);
 });
 
 Route::get('/contact', function () {
