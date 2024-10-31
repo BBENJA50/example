5afdb020ebe79c2c478e842b89ac1264cb404cb7
bbenja50 <migom@hotmail.be>
2024-10-31 17:34:31 +0100

    added seeder for jobs

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,11 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
+            'first_name' => 'Benjamin',
+            'last_name' => 'Migom',
             'email' => 'test@example.com',
         ]);
+
+        $this->call(JobSeeder::class);
     }
 }
---
+++ database/seeders/JobSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\job;
+
+use Illuminate\Database\Seeder;
+
+
+class JobSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Job::factory(200)->create();
+    }
+}
