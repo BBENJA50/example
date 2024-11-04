d1fc761b20fdfdbcee4d01ef00c4d4cfbd74ebb6
bbenja50 <migom@hotmail.be>
2024-11-04 14:36:02 +0100

    added CRUD for jobs and fixed routing issue

--- routes/web.php
+++ routes/web.php
@@ -7,11 +7,10 @@ use App\Models\Job;
 
 Route::get('/', function () {
 
-    $jobs = Job::all();
-
     return view('home');
 });
 
+// Index
 Route::get('/jobs', function () {
     $jobs = Job::with('employer')->latest()->simplePaginate(3); //prevents lazy loading
 
@@ -20,11 +19,12 @@ Route::get('/jobs', function () {
     ]);
 });
 
+// Create
 Route::get('/jobs/create', function () {
     return view('jobs.create');
 });
 
-
+// Show
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
 
@@ -33,6 +33,7 @@ Route::get('/jobs/{id}', function ($id) {
     ]);
 });
 
+// Store
 Route::post('/jobs', function () {
     request()->validate([
         'title' => ['required', 'min:3'],
@@ -49,6 +50,46 @@ Route::post('/jobs', function () {
     return redirect('/jobs');
 });
 
+// Edit
+Route::get('/jobs/{id}/edit', function ($id) {
+    $job = Job::find($id);
+
+    return view('jobs.edit', [
+        'job' => $job
+    ]);
+});
+
+// Update
+Route::patch('/jobs/{id}', function ($id) {
+    // validate
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required'],
+    ]);
+
+    // authorize (on hold)
+
+    //update job
+    $job = Job::findOrFail($id);
+
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary'),
+    ]);
+
+    return redirect('/jobs/' . $job->id);
+});
+
+// Destroy
+Route::delete('/jobs/{id}', function ($id) {
+    // authorize (on hold)
+
+    //delete job
+    Job::findOrFail($id)->delete();
+
+    //redirect
+    return redirect('/jobs');
+});
 
 Route::get('/contact', function () {
     return view('contact');
