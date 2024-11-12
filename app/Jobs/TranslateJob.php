4984567d9d32ba3e31f92e2dde2fb8565fa8e23d
bbenja50 <migom@hotmail.be>
2024-11-12 12:26:12 +0100

    fixed namespace for translateJob

--- app/Jobs/TranslateJob.php
+++ app/Jobs/TranslateJob.php
@@ -4,11 +4,13 @@ namespace App\Jobs;
 
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
-use Illuminate\Queue\Jobs\Job;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use App\Models\Job;
 
 class TranslateJob implements ShouldQueue
 {
-    use Queueable;
+    use Queueable, InteractsWithQueue, SerializesModels;
 
     /**
      * Create a new job instance.
@@ -23,6 +25,6 @@ class TranslateJob implements ShouldQueue
      */
     public function handle(): void
     {
-        logger('translating ' . $this->jobListing->title . ' to spanish.');
+        logger('translating ' . $this->jobListing->title . ' to Spanish.');
     }
 }
