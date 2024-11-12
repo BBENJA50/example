5b15059a636d5826f8caef8735ffe8069efd7b6a
bbenja50 <migom@hotmail.be>
2024-11-12 12:57:57 +0100

    added vite and npm.

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
     <x-slot:heading>
-        About page
+        Jobs page
     </x-slot:heading>
     <div class="space-y-4">
         @foreach ($jobs as $job)
@@ -9,7 +9,7 @@
                     {{ $job->employer->name }}
                 </div>
                 <div>
-                    <strong>{{ $job['title'] }}</strong> Pays {{ $job['salary'] }} per year.
+                    <strong class="text-laracasts">{{ $job['title'] }}</strong> Pays {{ $job['salary'] }} per year.
                 </div>
             </a>
         @endforeach
